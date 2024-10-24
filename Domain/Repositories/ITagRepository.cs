dae6e420827ba484eb71c3c3c9eee6856c03b4cc
Rob Cook <message@robcook.me.uk>
2024-10-24 10:52:01 +0100

    Tag domain classes and infrastructure stub.

---
+++ Domain/Repositories/ITagRepository.cs
@@ -0,0 +1,14 @@
+using Desk.Domain.Entities;
+
+namespace Desk.Domain.Repositories;
+
+public interface ITagRepository
+{
+    Task<Tag> GetByIdAsync(int id, CancellationToken ct);
+    
+    Task<List<Tag>> GetAllAsync(CancellationToken ct);
+
+    Task AddAsync(Tag tag, CancellationToken ct);
+
+    Task DeleteAsync(int id, CancellationToken ct);
+}
