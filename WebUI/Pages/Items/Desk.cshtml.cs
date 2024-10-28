3edab2ea1c4a11e9f195249d51b1f6f05c10f07f
Rob Cook <message@robcook.me.uk>
2024-10-28 11:58:40 +0000

    Empty pages for miniatures items.

---
+++ WebUI/Pages/Items/Desk.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Desk.WebUI.Pages.Items;
+
+public class DeskModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
---
+++ WebUI/Pages/Items/Pile.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Desk.WebUI.Pages.Items;
+
+public class PileModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
