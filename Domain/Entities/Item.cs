4b443b88b1d845195e4b857a3b3c79c406029b7c
Rob Cook <message@robcook.me.uk>
2024-11-08 15:41:47 +0000

    Migrations for image name on item.

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -64,7 +64,7 @@ public class Item
 
     public DateTimeOffset? UpdatedOn { get; protected set; }
 
-    public string ImageName { get; set; }
+    public string? ImageName { get; set; }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     
